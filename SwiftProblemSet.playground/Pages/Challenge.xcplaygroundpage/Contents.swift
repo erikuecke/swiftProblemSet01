c96a13bcc3ef693e901636304e6fa1a0e004ee09
Erik Uecke <erikuecke@gmail.com>
2017-06-11 19:25:11 -0400

    Finish Swift Problem Set

---
+++ SwiftProblemSet.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+/*:
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous)
+ ****
+ */
+import Foundation
+//: ## Challenge
+//: **This exercise is completely optional and is not required for completing the Swift Problem Set.**
+//: ### Challenge 1
+//: Mystery code! What does this code do? Briefly, using comments, describe what is happening in each line. **Hint**: You may need to look up [Int initializers](http://stackoverflow.com/questions/30739460/toint-removed-in-swift-2).
+// Created an array of strings.
+let array = ["A", "13", "B", "5", "87", "t", "41"]
+
+// Created an Int variable
+var sum = 0
+
+// For each string in the array...
+for string in array {
+    
+    // Conditional checking to see if each string in the array can be an Int type and hold a value if converted
+    if Int(string) != nil {
+        // Force unwrapping the string value to an Int type constant
+        let intToAdd = Int(string)!
+        // Adding the int value of the string to the sum
+        sum += intToAdd
+    }
+}
+// print the sum of the strings that were numbers in the array.
+print(sum)
+
+/*:
+ ****
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous)
+ */
---
+++ SwiftProblemSet.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+/*:
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
+ ****
+ */
+//: ## Variables
+/*:
+ ### Exercise 1
+ 
+ For each of the following names and types, create a variable or constant that best represents it:
+ 
+ - gallonsRemainingInTank (`Double`)
+ - ageOfSpecimenInWeeks (`Int`)
+ - birthplace (`String`)
+ - myFirstProgrammingLanguage (`String`)
+ */
+var gallonsRemainingInTank: Double
+var ageOfSpecimenInWeeks: Int
+let birthplace: String
+let myFirstProgrammingLangue: String = "Javascript"
+//: ### Exercise 2
+//: Make the minimal changes necessary to fix the following compiler errors related to assignment, variables, and constants.
+var favoriteLanguage: String = "Swift"
+gallonsRemainingInTank = 2
+ageOfSpecimenInWeeks = 32
+let bestProgrammingLanguage: String = favoriteLanguage
+//: ### Exercise 3
+//: Below is some code to reverse a string. We've declared `stringToReverse` and `arrayOfCharacters` as variables. Rewrite this code to reverse a string using only constants declared with `let`, no variables. **Note**: In the new version you may use as many constants as you'd like.
+let stringToReverse = "Mutable or Immutable? That is the question."
+let arrayOfCharacters = stringToReverse.characters.reversed()
+let reversedString = String(arrayOfCharacters)
+/*: 
+### Exercise 4
+
+ For the following descriptions, create a variable or constant that best represents it:
+
+- an `Int` value representing the hunger level of an animal over time.
+- an `Int` value representing the score always required to win a game.
+- a `String` value representing a student’s birthday.
+- a `String` value representing what a user types into the status update area.
+*/
+var hungerLevel = 5
+let scoreToWin = 10
+let studentBirthday = "July 4 1985"
+var statusUpdate = "Im at school"
+/*:
+ ****
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
+ */
